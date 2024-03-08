6b5dca01bd0506ca4e4428e00aea7b1eac078517
rir3 <rafy.reyes.007@gmail.com>
2024-03-08 18:13:51 -0500

    added moviepy

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from gpiozero import Button
+import moviepy
 
+#Relay Activated Auto Recording
+#This program will start recording when Pin 11 GPIO 17 is grounded.
+#This can be activated with a relay.
+
+#Trigger
 button = Button(17)
 button.wait_for_press()
 print("The button was pressed!")
+
+
+#Record
+
+
+
+#Logo Embeding
+
+
+
+
+#Raspberry Pi Nas
