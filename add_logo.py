c04a472b01dbc6f0726f7eda35ac37608c9d0290
rir3 <rafy.reyes.007@gmail.com>
2024-03-12 17:41:22 -0400

    fixing error

--- add_logo.py
+++ add_logo.py
@@ -1,7 +1,7 @@
 from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 
 # Load the video clip
-video_clip = VideoFileClip("video.mp4", fps=30)
+video_clip = VideoFileClip("video.mp4")
 
 # Load the logo image (adjust dimensions as needed)
 logo = ImageClip("logo.png").resize(height=500)
@@ -13,4 +13,4 @@ logo = logo.set_duration(video_clip.duration)
 final_clip = CompositeVideoClip([video_clip, logo.set_position(("right", "bottom"))])
 
 # Write the final clip to a new file
-final_clip.write_videofile("logo_video.mp4")
+final_clip.write_videofile("logo_video.mp4", codec="libx264", fps=video_clip.fps)
