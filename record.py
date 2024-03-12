3450b4521dfc7498a99aded1bf09026735764fd6
rir3 <rafy.reyes.007@gmail.com>
2024-03-12 15:50:59 -0400

    Updated for picamera2

--- record.py
+++ record.py
@@ -1,7 +1,4 @@
-import picamera2 as picamera 
+from picamera2 import Picamera2
 
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
-camera.start_recording('my_video.h264')
-camera.wait_recording(60)
-camera.stop_recording()
+picam2 = Picamera2()
+picam2.start_and_record_video("test.mp4", duration=5)
