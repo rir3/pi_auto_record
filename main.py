2f1815983af547c53f1acecbe08852c00693ea9c
rir3 <rafy.reyes.007@gmail.com>
2024-03-14 16:57:38 -0400

    added main function to main.py

--- main.py
+++ main.py
@@ -16,32 +16,33 @@ timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 output_video = f"output_video_{timestamp}.mp4"
 recording_duration = 10
 
-#Trigger
-button = Button(17)
-logging.info("Waiting for button press.")
-#button.wait_for_press()
-logging.info("The button was pressed!")
+def main():
+    #Trigger
+    button = Button(17)
+    logging.info("Waiting for button press.")
+    #button.wait_for_press()
+    logging.info("The button was pressed!")
 
 
-#Record
-logging.info("Recording Started.")
-picam2 = Picamera2()
-picam2.start_and_record_video(video, duration=recording_duration)
+    #Record
+    logging.info("Recording Started.")
+    picam2 = Picamera2()
+    picam2.start_and_record_video(video, duration=recording_duration)
 
-#Logo Embeding
-logging.info("Logo Embeding Started.")
-ffmpeg_command = [
-    'ffmpeg',
-    '-i', video,
-    '-i', image,
-    '-filter_complex', '[1:v]scale=250:-1[logo];[0:v][logo]overlay=W-w-10:H-h-10',
-    '-c:a', 'copy',
-    dir+output_video
-]
-process = subprocess.run(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #Logo Embeding
+    logging.info("Logo Embeding Started.")
+    ffmpeg_command = [
+        'ffmpeg',
+        '-i', video,
+        '-i', image,
+        '-filter_complex', '[1:v]scale=250:-1[logo];[0:v][logo]overlay=W-w-10:H-h-10',
+        '-c:a', 'copy',
+        dir+output_video
+    ]
+    process = subprocess.run(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-if process.returncode == 0:
-    logging.info("FFmpeg command executed successfully")
-else:
-    logging.info(f"FFmpeg command failed with error: {process.stderr.decode('utf-8')}")
+    if process.returncode == 0:
+        logging.info("FFmpeg command executed successfully")
+    else:
+        logging.info(f"FFmpeg command failed with error: {process.stderr.decode('utf-8')}")
 
